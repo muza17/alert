96962d519d18cb1d24f60882649ee8a9f5e1f836
muza17 <inomovamahfuza404@gmail.com>
2021-11-02 12:50:04 +0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+console.log("Assalomu alykum! Here is some information");
+//prompts
+var programmerName = prompt ("First name: ");
+var programmerSurName = prompt ("First surname: ");
+var programmerAge = prompt ("Your age: ");
+var programmerAdress = prompt ("Your current adress: ");
+var programmerBranch = prompt ("Your branch (in which you are working): ");
+var programmerGrade = prompt ("Your current grade (junior/middle/senior): ");
+var programmerLanguage = prompt ("IT Languages you can use easily : ");
+var programmerNative = prompt ("Your nationality: ");
+var programmerSalary = prompt ("Your current salary: ");
+var programmerWorkAdress = prompt ("Adress you work: ");
+
+//consoles
+
+console.log(
+    "Informations about programmer: "+ "\n"
+    + "  Programmer's surname: " + programmerName + "\n"
+    + "  Programmer's surname: " + programmerSurName + "\n"
+    + "  Programmer's age: " + programmerAge + "\n"
+    + "  Programmer's adress: " + programmerAdress + "\n"
+    + "  Programmer's branch: " + programmerBranch + "\n"
+    + "  Programmer's grade: " + programmerGrade + "\n"
+    + "  Programmer's IT language: " + programmerLanguage + "\n"
+    + "  Programmer's native: " + programmerNative + "\n"
+    + "  Programmer's salary: " + programmerSalary + "\n"
+    + "  Programmer's work adress: " + programmerWorkAdress + "\n"
+);
+//alerts
+alert(
+    "Informations about programmer: "+ "\n"
+    + "  Programmer's surname: " + programmerName + "\n"
+    + "  Programmer's surname: " + programmerSurName + "\n"
+    + "  Programmer's age: " + programmerAge + "\n"
+    + "  Programmer's adress: " + programmerAdress + "\n"
+    + "  Programmer's branch: " + programmerBranch + "\n"
+    + "  Programmer's grade: " + programmerGrade + "\n"
+    + "  Programmer's language: " + programmerLanguage + "\n"
+    + "  Programmer's native: " + programmerNative + "\n"
+    + "  Programmer's salary: " + programmerSalary + "\n"
+    + "  Programmer's work adress: " + programmerWorkAdress + "\n"
+);
